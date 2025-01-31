ecca5cc5fe8b86b15061ea947d12c4c7f168d0ba
Sami-C4C <101250244+Sami-C4C@users.noreply.github.com>
2025-01-31 11:15:28 +0100

    Update settings.py
    Secret_Key is removed

--- resume_generator/settings.py
+++ resume_generator/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y%lqpy)2p3=@0#c=_7^%6mm1r@)n3ax6!qg_p%u#ar9zhx!@!&'
+SECRET_KEY = 'Here your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
